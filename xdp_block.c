c72760a87f127d61059b398f8a55936c318bec68
slimane-msb <slimanemesbah0@gmail.com>
2026-03-20 07:48:38 +0100

    xdp on one ip

---
+++ xdp_block.c
@@ -0,0 +1,33 @@
+#include <linux/bpf.h>
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, __u32);
+    __type(value, __u8);
+} blacklist SEC(".maps");
+
+SEC("xdp")
+int xdp_prog(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    struct ethhdr *eth = data;
+
+    if ((void *)(eth + 1) > data_end) return XDP_PASS;
+    if (eth->h_proto != bpf_htons(ETH_P_IP)) return XDP_PASS;
+
+    struct iphdr *iph = (void *)(eth + 1);
+    if ((void *)(iph + 1) > data_end) return XDP_PASS;
+
+    __u32 src_ip = iph->saddr;
+    if (bpf_map_lookup_elem(&blacklist, &src_ip)) return XDP_DROP;
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
