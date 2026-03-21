5ed5a6d4dd3411179f1a30db905368e85b177888
slimane-msb <slimanemesbah0@gmail.com>
2026-03-21 12:26:50 +0100

    L7 perfect, todo : link it to deamon

--- sinkhole/setup.sh
+++ sinkhole/setup.sh
@@ -1,18 +1,3 @@
-sudo iptables -I OUTPUT  -p tcp --dport 80  -j NFQUEUE --queue-num 0
-sudo iptables -I OUTPUT  -p tcp --dport 443 -j NFQUEUE --queue-num 0
-sudo iptables -I OUTPUT  -p udp --dport 53  -j NFQUEUE --queue-num 0
-sudo iptables -I OUTPUT  -p udp --dport 443 -j NFQUEUE --queue-num 0
-sudo iptables -I FORWARD -p tcp --dport 80  -j NFQUEUE --queue-num 0
-sudo iptables -I FORWARD -p tcp --dport 443 -j NFQUEUE --queue-num 0
-sudo iptables -I FORWARD -p udp --dport 53  -j NFQUEUE --queue-num 0
-sudo iptables -I FORWARD -p udp --dport 443 -j NFQUEUE --queue-num 0
-
-
-sudo ./target/release/sinkhole ledvance.ewyse.agency atun.com
-
-
-
-
 curl atun.com
 wget atun.com
 
@@ -24,8 +9,3 @@ curl http://atun.com
 
 ping ledvance.ewyse.agency 
 ping polyglotte-institute.eu 
-
-
-
-
-sudo iptables -D OUTPUT  -p tcp --dport 80  -j NFQUEUE --queue-num 0
